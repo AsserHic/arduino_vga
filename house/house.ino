1489392361a0ddbc52396179a6ec4f1ce3594b1c
Marko Laakso <marko.laakso@valuemotive.com>
2021-03-30 18:38:35 +0300

    Skeleton

--- house/house.ino
+++ house/house.ino
@@ -4,26 +4,46 @@
 VGAX vga;
 
 void setup() {
-  vga.begin();
+  vga.begin(false);
 }
 
 void loop() {
   draw_house();
-  
+  vga.delay(4000);
+  vga.clear(0);
+  vga.delay(1000);
+}
+
+void line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1) {
+  VGAXUtils::draw_line(x0,  y0, x1, y1, 2);
+  vga.delay(1000);
 }
 
 void draw_house() {
-  unsigned int x_left      = 0;
-  unsigned int x_right     = 29;
-  unsigned int x_wall_left = x_left + eaves_width;
-  unsigned int x_mid       = (X_RIGHT - X_LEFT) / 2;
-  
-  unsigned int y_top       = 0;
-  unsigned int y_ground    = 59;
+  uint8_t eaves_width = 4;
 
-  VGAXUtils::draw_line(x_wall_left,  y_ground, x_wall_left,  Y_ROOF);
-  VGAXUtils::draw_line(x_wall_right, y_ground, X_WALL_RIGHT, Y_ROOF);
-  VGAXUtils::draw_line(x_left,       Y_ROOF,   x_right,      Y_ROOF);
-  VGAXUtils::draw_line(x_left,       Y_ROOF,   X_MID,        Y_TOP);
-  VGAXUtils::draw_line(x_right,      Y_ROOF,   X_MID,        Y_TOP);
+  uint8_t x_left          = 0;
+  uint8_t x_right         = 119;
+  uint8_t x_wall_left     = x_left + eaves_width;
+  uint8_t x_wall_right    = x_right - eaves_width;
+  uint8_t x_mid           = (x_right - x_left) / 2;
+  uint8_t x_chimney_left  = x_mid / 3;
+  uint8_t x_chimney_right = x_chimney_left * 2;
+
+  uint8_t y_roof           = 20;
+  uint8_t y_top            = 0;
+  uint8_t y_ground         = 59;
+  uint8_t y_chimney_top    = 3;
+  uint8_t y_chimney_down_l = 13;
+  uint8_t y_chimney_down_r = 8;
+
+  line(x_wall_left,  y_ground, x_wall_left,  y_roof);
+  line(x_wall_right, y_ground, x_wall_right, y_roof);
+  line(x_left,       y_roof,   x_right,      y_roof);
+  line(x_left,       y_roof,   x_mid,        y_top);
+  line(x_right,      y_roof,   x_mid,        y_top);
+  
+  line(x_chimney_left,  y_chimney_top,    x_chimney_right, y_chimney_top);
+  line(x_chimney_left,  y_chimney_down_l, x_chimney_left,  y_chimney_top);
+  //line(x_chimney_right, y_chimney_down_r, x_chimney_right, y_chimney_top);
 }
